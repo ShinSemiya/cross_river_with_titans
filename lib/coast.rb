b6877674997e4065a7160d9550c75d31a7bd0562
jun-onishi <jun_you@live.jp>
2013-08-07 15:01:23 +0900

    implement max_number and matrix

--- lib/coast.rb
+++ lib/coast.rb
@@ -4,11 +4,28 @@ class Coast
   # numberにより配置される兵士と巨人の数を可変としている
   # この実装ではSoldierとTitanと数は同数しか対応していない
   def initialize(number = 3)
-    @max_number = 3
+    @max_number = number
     @matrix     = set_up_matrix(number)
   end
 
-  def set_up_matrix(max_number)
-    []
+  def set_up_matrix(matrix_size)
+    matrix = []
+    0.upto(matrix_size) {|y| matrix << set_up_row(y) }
+    matrix
+  end
+
+  def set_up_row(y)
+    row = []
+    0.upto(max_number) {|x| row << set_up_cell(x, y) }
+    row
+  end
+
+  def set_up_cell(x, y)
+    if y == 0 ||
+       y == max_number ||
+       y == x
+      return 1
+    end
+    0
   end
 end
--- spec/lib/coast_spec.rb
+++ spec/lib/coast_spec.rb
@@ -2,9 +2,44 @@ require 'spec_helper'
 
 describe Coast do
   let!(:coast){ Coast.new(3) }
+
   describe "#initialize" do
     it "max_number is 3" do
       coast.max_number.should == 3
+    end
+
+    it "matrix is initialized" do
+      coast.matrix.should == [[1, 1, 1, 1],
+                              [0, 1, 0, 0],
+                              [0, 0, 1, 0],
+                              [1, 1, 1, 1],]
+    end
+  end
+
+  describe "#set_up_matrix" do
+    context "max_number is 5" do
+      let!(:max_size){ 2 }
+
+      it "matrix is initialized" do
+        coast = Coast.new(max_size)
+        coast.set_up_matrix(max_size).should == [[1, 1, 1],
+                                                 [0, 1, 0],
+                                                 [1, 1, 1],]
+      end
+    end
+
+    context "max_number is 5" do
+      let!(:max_size){ 5 }
+
+      it "matrix is initialized" do
+        coast = Coast.new(max_size)
+        coast.set_up_matrix(max_size).should == [[1, 1, 1, 1, 1, 1],
+                                                 [0, 1, 0, 0, 0, 0],
+                                                 [0, 0, 1, 0, 0, 0],
+                                                 [0, 0, 0, 1, 0, 0],
+                                                 [0, 0, 0, 0, 1, 0],
+                                                 [1, 1, 1, 1, 1, 1],]
+      end
     end
   end
 end
